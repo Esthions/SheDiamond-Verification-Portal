6b9cc4a7aa42dcf66746818846c37f068d354750
Esthions <esther.omuedi@gmail.com>
2026-06-18 16:48:15 +0100

    Remove invitation check and add verification alert

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-SheDiamond Executive Event
-            ;
-            result.style.color = "green";
-
-        } else {
-            result.innerHTML = 
-                ⚠️ <b>FOUND BUT NOT CONFIRMED</b><br><br>
-                👤 Name: ${name}<br>
-                📱 Phone: ${phone}<br>
-                🕒 Status: Pending Approval
-            `;
-            result.style.color = "orange";
-        }
-
-    } catch (error) {
-        result.innerHTML = "Error checking invitation. Please try again.";
-        result.style.color = "red";
-    }
+function verifyInvitation() {
+    alert("Button works");
 }
