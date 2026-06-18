72cf1cd8346e467660493feb8b89b04849d25a7e
Esthions <esther.omuedi@gmail.com>
2026-06-18 23:56:58 +0100

    Convert verifyInvitation to an async function

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function verifyInvitation() {
+async function verifyInvitation() {
     document.getElementById("result").innerHTML = "Checking...";
-    alert("Function is running");
+
+    const response = await fetch("https://opensheet.elk.sh/1-tpFvrD1CMh61jNb94rNL8KMQqcVqfxvEx2iGmj90CU/Sheet1");
+    const data = await response.json();
+
+    alert("Rows found: " + data.length);
 }
